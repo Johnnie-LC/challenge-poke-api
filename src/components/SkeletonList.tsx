7c4dd871612aa7a1d10f08291029468395323343
johnnieLopez <johnnie.lopez@foxordering.com>
2024-11-20 09:35:47 -0500

    fix styles

--- src/components/SkeletonList.tsx
+++ src/components/SkeletonList.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/react-in-jsx-scope */
+import Pagination from "./Pagination";
 import SkeletonItem from "./SkeletonItem";
 
 const SkeletonList: React.FC = () => {
   return (
-    <div className="skeleton-list">
-      {Array.from({ length: 20 }).map((_, index) => (
-        <SkeletonItem key={index} />
-      ))}
-    </div>
+    <>
+      <div className="skeleton-list">
+        {Array.from({ length: 20 }).map((_, index) => (
+          <SkeletonItem key={index} />
+        ))}
+      </div>
+      <Pagination />
+    </>
   );
 };
 
